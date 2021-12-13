de88cdb17f4c8c317673b9d279299dad794838e5
rahul1984p <30750717+rahul1984p@users.noreply.github.com>
2021-12-13 23:07:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,8 +4,5 @@ provider "aws" {
 
 module "s3-webapp" {
   source  = "app.terraform.io/Rahul-module/s3-webapp/aws"
-  name        = var.name
-  region = var.region
-  prefix = var.prefix
   version = "1.0.0"
 }
